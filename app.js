606d98ad3267954a6ac1d8aa223b9839a34e4324
Parasite-231 <muktadir@iut-dhaka.edu>
2023-02-09 12:35:39 +0600

    final

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express()
+dotenv.config()
 
 app.get("/",(req,res)=>{
 
     res.send("Welcome Azure")
 })
 
+port =process.env.PORT
 
-app.listen(2136,()=>{
+app.listen(port,()=>{
 
-    console.log("I am listening, sir, are you listening ?  !")
+    console.log("I am listening, sir, are you listening ?  !"+port)
 })
